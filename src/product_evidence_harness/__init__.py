0a59f21c4ef1692b0fc83010fdcad7e3aa9a9a94
Akilan km <66872547+Akilankm@users.noreply.github.com>
2026-07-11 22:50:53 +0530

    Expose one-credit workflow as public API

--- src/product_evidence_harness/__init__.py
+++ src/product_evidence_harness/__init__.py
@@ -21,39 +21,50 @@ from src.product_evidence_harness.contracts import (
     URLCandidate,
 )
 from src.product_evidence_harness.country_profiles import CountryProfile, CountryProfileRegistry, LanguageProfile
-from src.product_evidence_harness.elite import (
-    CodingReadiness,
-    ConfidenceBreakdown,
-    EnterpriseEvidenceAssessment,
-    EnterpriseEvidenceEngine,
+from src.product_evidence_harness.detectors import DetectorFinding, VariantConflictDetector
+from src.product_evidence_harness.elite import CodingReadiness, ConfidenceBreakdown, EnterpriseEvidenceAssessment, EnterpriseEvidenceEngine
+from src.product_evidence_harness.feature_evidence import EvidenceSetSelector, FeatureAwareEvidenceExtractor, FeatureReasoner
+from src.product_evidence_harness.feature_schema import (
+    EvidenceSetDecision,
+    FeatureCriticality,
+    FeatureDefinition,
+    FeatureEvidence,
+    FeatureEvidenceStatus,
+    FeatureSchema,
+    URLFeatureAssessment,
 )
 from src.product_evidence_harness.feedback import ReviewFeedbackRecord, ReviewFeedbackStore, RetailerDomainMemory
-from src.product_evidence_harness.identity_verifier import ProductIdentityVerifier
 from src.product_evidence_harness.identity import ProductIdentityGraph, ProductIdentityGraphBuilder
-from src.product_evidence_harness.detectors import DetectorFinding, VariantConflictDetector
+from src.product_evidence_harness.identity_verifier import ProductIdentityVerifier
 from src.product_evidence_harness.io import CSVProductIO
 from src.product_evidence_harness.llm import ExactProductLLMAdjudicator, LLMConfig, LLMService
 from src.product_evidence_harness.logging_utils import RichPrinter, configure_logging
-from src.product_evidence_harness.tournament_pipeline import (
+from src.product_evidence_harness.one_credit_pipeline import (
+    FeatureAwareHarnessResult,
     HarnessProductURLFinderPipeline,
     HybridProductURLFinderPipeline,
+    OneCreditConfig,
+    OneCreditProductEvidenceHarness,
     ProductEvidenceHarness,
 )
 from src.product_evidence_harness.production_url import ProductionURLAssessment, ProductionURLGate
 from src.product_evidence_harness.ranker import ProductURLRanker
 from src.product_evidence_harness.scraper import CrawlScraper
+from src.product_evidence_harness.tournament_pipeline import TournamentAwareProductEvidenceHarness as LegacyTournamentProductEvidenceHarness
 
 __all__ = [
-    "SerpAPIConfig", "HarnessConfig", "HarnessPolicy", "HarnessBudgetConfig",
+    "SerpAPIConfig", "HarnessConfig", "HarnessPolicy", "HarnessBudgetConfig", "OneCreditConfig",
     "DiscoveryMode", "ProductQuery", "ProductURLMatch", "HarnessTrace", "PipelineTrace",
-    "URLCandidate", "LLMSearchPlan", "LLMSearchQuery", "CandidateScorecard", "ScoredURLCandidate", "ScrapeResult", "ProductEvidence",
-    "MatchVerification", "OrganicSearchResponse", "OrganicSearchResult", "SerpAIResponse",
-    "ProductSearchState", "AgentAction", "AgentActionRecord", "ProductEvidenceHarness",
-    "HarnessProductURLFinderPipeline", "HybridProductURLFinderPipeline", "CrawlScraper",
-    "ProductIdentityVerifier", "ProductIdentityGraph", "ProductIdentityGraphBuilder", "DetectorFinding", "VariantConflictDetector", "ProductURLRanker", "RichPrinter", "configure_logging", "CSVProductIO",
-    "CountryProfile", "CountryProfileRegistry", "LanguageProfile",
-    "ExactProductLLMAdjudicator", "LLMConfig", "LLMService",
+    "URLCandidate", "LLMSearchPlan", "LLMSearchQuery", "CandidateScorecard", "ScoredURLCandidate",
+    "ScrapeResult", "ProductEvidence", "MatchVerification", "OrganicSearchResponse",
+    "OrganicSearchResult", "SerpAIResponse", "ProductSearchState", "AgentAction", "AgentActionRecord",
+    "ProductEvidenceHarness", "OneCreditProductEvidenceHarness", "FeatureAwareHarnessResult",
+    "HarnessProductURLFinderPipeline", "HybridProductURLFinderPipeline", "LegacyTournamentProductEvidenceHarness",
+    "FeatureDefinition", "FeatureSchema", "FeatureCriticality", "FeatureEvidenceStatus", "FeatureEvidence",
+    "URLFeatureAssessment", "EvidenceSetDecision", "FeatureAwareEvidenceExtractor", "EvidenceSetSelector", "FeatureReasoner",
+    "CrawlScraper", "ProductIdentityVerifier", "ProductIdentityGraph", "ProductIdentityGraphBuilder",
+    "DetectorFinding", "VariantConflictDetector", "ProductURLRanker", "RichPrinter", "configure_logging", "CSVProductIO",
+    "CountryProfile", "CountryProfileRegistry", "LanguageProfile", "ExactProductLLMAdjudicator", "LLMConfig", "LLMService",
     "EnterpriseEvidenceEngine", "EnterpriseEvidenceAssessment", "ConfidenceBreakdown", "CodingReadiness",
-    "ReviewFeedbackRecord", "ReviewFeedbackStore", "RetailerDomainMemory",
-    "ProductionURLGate", "ProductionURLAssessment",
+    "ReviewFeedbackRecord", "ReviewFeedbackStore", "RetailerDomainMemory", "ProductionURLGate", "ProductionURLAssessment",
 ]
