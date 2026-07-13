ec60a29779b0335f68648505765b9f40364dc2e9
Akilan km <66872547+Akilankm@users.noreply.github.com>
2026-07-13 12:42:20 +0530

    Expose strict three-stage production API

--- src/product_evidence_harness/__init__.py
+++ src/product_evidence_harness/__init__.py
@@ -23,9 +23,10 @@ from src.product_evidence_harness.contracts import (
 from src.product_evidence_harness.country_profiles import CountryProfile, CountryProfileRegistry, LanguageProfile
 from src.product_evidence_harness.detectors import DetectorFinding, VariantConflictDetector
 from src.product_evidence_harness.elite import CodingReadiness, ConfidenceBreakdown, EnterpriseEvidenceAssessment, EnterpriseEvidenceEngine
-from src.product_evidence_harness.environment import (
-    EnvironmentValidationError,
+from src.product_evidence_harness.environment import EnvironmentValidationError
+from src.product_evidence_harness.three_stage_environment import (
     EnvironmentValidationReport,
+    ThreeStageEnvironmentValidationReport,
     validate_runtime_environment,
 )
 from src.product_evidence_harness.feature_evidence import EvidenceSetSelector, FeatureAwareEvidenceExtractor, FeatureReasoner
@@ -51,11 +52,10 @@ from src.product_evidence_harness.llm import ExactProductLLMAdjudicator, LLMConf
 from src.product_evidence_harness.llm.feature_reasoner import LLMFeatureReasoner
 from src.product_evidence_harness.llm.vision_reasoner import MultimodalFeatureReasoner, VisionReasonerConfig
 from src.product_evidence_harness.logging_utils import RichPrinter, configure_logging
-from src.product_evidence_harness.one_credit_pipeline import (
-    FeatureAwareHarnessResult,
-    OneCreditConfig,
-    OneCreditProductEvidenceHarness,
-)
+from src.product_evidence_harness.one_credit_pipeline import FeatureAwareHarnessResult, OneCreditConfig, OneCreditProductEvidenceHarness
+from src.product_evidence_harness.three_stage_pipeline import ThreeStageProductEvidenceHarness
+from src.product_evidence_harness.strict_acceptance import PrimaryURLAcceptance, StrictPrimaryURLSelector
+from src.product_evidence_harness.url_durability import ProductURLDurabilityGate, URLDurabilityAssessment
 from src.product_evidence_harness.production_url import ProductionURLAssessment, ProductionURLGate
 from src.product_evidence_harness.ranker import ProductURLRanker
 from src.product_evidence_harness.scraper import CrawlScraper
@@ -74,10 +74,12 @@ from src.product_evidence_harness.browser_contracts import (
 )
 from src.product_evidence_harness.browser_client import BrowserEvidenceClient, BrowserServiceConfig, BrowserServiceError
 from src.product_evidence_harness.agent_service.orchestrator import AgentRuntimeConfig, FeatureSetRegistry, ProductEvidenceOrchestrator
+from src.product_evidence_harness.agent_service.strict_orchestrator import StrictProductEvidenceOrchestrator
 
 apply_compatibility_patches()
 
-FeatureAwareProductEvidenceHarness = OneCreditProductEvidenceHarness
+FeatureAwareProductEvidenceHarness = ThreeStageProductEvidenceHarness
+LegacyOneCreditProductEvidenceHarness = OneCreditProductEvidenceHarness
 LegacyTournamentProductEvidenceHarness = TournamentAwareProductEvidenceHarness
 
 __all__ = [
@@ -86,20 +88,22 @@ __all__ = [
     "URLCandidate", "LLMSearchPlan", "LLMSearchQuery", "CandidateScorecard", "ScoredURLCandidate",
     "ScrapeResult", "ProductEvidence", "MatchVerification", "OrganicSearchResponse",
     "OrganicSearchResult", "SerpAIResponse", "ProductSearchState", "AgentAction", "AgentActionRecord",
-    "ProductEvidenceHarness", "FeatureAwareProductEvidenceHarness", "OneCreditProductEvidenceHarness",
-    "FeatureAwareHarnessResult", "HarnessProductURLFinderPipeline", "HybridProductURLFinderPipeline",
-    "LegacyTournamentProductEvidenceHarness", "FeatureDefinition", "FeatureSchema", "FeatureCriticality",
-    "FeatureEvidenceStatus", "FeatureEvidence", "URLFeatureAssessment", "EvidenceSetDecision",
-    "FeatureAwareEvidenceExtractor", "EvidenceSetSelector", "FeatureReasoner", "load_feature_schema",
-    "CrawlScraper", "ProductIdentityVerifier", "ProductIdentityGraph", "ProductIdentityGraphBuilder",
-    "DetectorFinding", "VariantConflictDetector", "ProductURLRanker", "RichPrinter", "configure_logging", "CSVProductIO",
-    "CountryProfile", "CountryProfileRegistry", "LanguageProfile", "ExactProductLLMAdjudicator", "LLMConfig", "LLMService",
-    "LLMFeatureReasoner", "MultimodalFeatureReasoner", "VisionReasonerConfig",
-    "EnvironmentValidationError", "EnvironmentValidationReport", "validate_runtime_environment",
+    "ProductEvidenceHarness", "FeatureAwareProductEvidenceHarness", "ThreeStageProductEvidenceHarness",
+    "LegacyOneCreditProductEvidenceHarness", "FeatureAwareHarnessResult", "HarnessProductURLFinderPipeline",
+    "HybridProductURLFinderPipeline", "LegacyTournamentProductEvidenceHarness", "FeatureDefinition",
+    "FeatureSchema", "FeatureCriticality", "FeatureEvidenceStatus", "FeatureEvidence",
+    "URLFeatureAssessment", "EvidenceSetDecision", "FeatureAwareEvidenceExtractor", "EvidenceSetSelector",
+    "FeatureReasoner", "load_feature_schema", "CrawlScraper", "ProductIdentityVerifier",
+    "ProductIdentityGraph", "ProductIdentityGraphBuilder", "DetectorFinding", "VariantConflictDetector",
+    "ProductURLRanker", "RichPrinter", "configure_logging", "CSVProductIO", "CountryProfile",
+    "CountryProfileRegistry", "LanguageProfile", "ExactProductLLMAdjudicator", "LLMConfig", "LLMService",
+    "LLMFeatureReasoner", "MultimodalFeatureReasoner", "VisionReasonerConfig", "EnvironmentValidationError",
+    "EnvironmentValidationReport", "ThreeStageEnvironmentValidationReport", "validate_runtime_environment",
     "EnterpriseEvidenceEngine", "EnterpriseEvidenceAssessment", "ConfidenceBreakdown", "CodingReadiness",
-    "ReviewFeedbackRecord", "ReviewFeedbackStore", "RetailerDomainMemory", "ProductionURLGate", "ProductionURLAssessment",
-    "AcquisitionMethod", "BrowserActionRecord", "BrowserEvidenceBundle", "BrowserEvidenceRequest",
-    "BrowserEvidenceStatus", "EvidenceIntent", "ProductIdentityPayload", "VisualAsset",
-    "BrowserEvidenceClient", "BrowserServiceConfig", "BrowserServiceError",
-    "AgentRuntimeConfig", "FeatureSetRegistry", "ProductEvidenceOrchestrator",
+    "ReviewFeedbackRecord", "ReviewFeedbackStore", "RetailerDomainMemory", "ProductionURLGate",
+    "ProductionURLAssessment", "StrictPrimaryURLSelector", "PrimaryURLAcceptance", "ProductURLDurabilityGate",
+    "URLDurabilityAssessment", "AcquisitionMethod", "BrowserActionRecord", "BrowserEvidenceBundle",
+    "BrowserEvidenceRequest", "BrowserEvidenceStatus", "EvidenceIntent", "ProductIdentityPayload", "VisualAsset",
+    "BrowserEvidenceClient", "BrowserServiceConfig", "BrowserServiceError", "AgentRuntimeConfig",
+    "FeatureSetRegistry", "ProductEvidenceOrchestrator", "StrictProductEvidenceOrchestrator",
 ]
